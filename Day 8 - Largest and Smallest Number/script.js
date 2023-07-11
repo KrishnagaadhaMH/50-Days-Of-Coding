9c22d79269171a9ace3e506a2787bdb1ac45e765
Krishnagaadha M H <81553805+KrishnagaadhaMH@users.noreply.github.com>
2023-07-11 21:47:48 +0530

    Add files via upload

---
+++ Day 8 - Largest and Smallest Number/script.js
@@ -0,0 +1,16 @@
+let arr = prompt("Enter an array of numbers: ");
+arr = arr.split(" ");
+const len = arr.length;
+var la = arr[0];
+var sm = arr[0];
+for (i=0; i<=len;i++){
+    if (arr[i]>la) {
+        la = arr[i];
+    }
+}
+for (i=0; i<=len;i++){
+    if(arr[i]<sm){
+        sm = arr[i];
+    }
+}
+console.log("Largest number is: "+ la +" smallest number is: "+sm);
